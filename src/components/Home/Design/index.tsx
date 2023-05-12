144946ae08e8ea1da466a24ac2176379bab5d195
m4tt133 <kapalamateusz81@gmail.com>
2023-05-12 12:53:37 +0200

    Fixes - Content

--- src/components/Home/Design/index.tsx
+++ src/components/Home/Design/index.tsx
@@ -10,9 +10,9 @@ export default function Design(): React.ReactElement{
             <div className="flex flex-col gap-10 w-1/2 lg:w-full">
                 <Heading subheading="Design & Creativity" heading="A Designer At Heart"/>
                 <div className="flex flex-col text-[14px] gap-4">
-                    <p className="font-semibold">I'm a developer and also designer.</p>
-                    <p>I've built my career around the idea that software should be beautiful, as well as functional, because it's not just about how things look—it's also about how they feel.</p>
-                    <p>I believe that design is more than just an aesthetic choice: it's an important part of how we interact with our world, and how we connect with each other. As such, I work hard to ensure that every project I take on is designed in such a way that makes users feel connected with the product and its creators.</p>
+                    <p className="font-semibold">I am a developer and also designer.</p>
+                    <p>I have built my career around the idea that software should be beautiful, as well as functional, because it is not just about how things look, but also about how they feel.</p>
+                    <p>I believe that design is more than just an aesthetic choice: it is an important part of how we interact with the digital world, and how we connect with each other. As such, I work hard to ensure that every project I take on is designed in such a way that makes users feel connected with the product and its creators.</p>
                 </div>
             </div>
             <div className="flex flex-col w-1/2 h-max gap-4 lg:w-full">
--- src/components/Home/Knowledge/index.tsx
+++ src/components/Home/Knowledge/index.tsx
@@ -13,9 +13,9 @@ export default function Knowledge(): React.ReactElement{
         <section className="wrapper-padding-y flex flex-col  gap-5">
             <Heading subheading="Knowledge" heading="I Understand Business and marketing"/>
             <div className="flex flex-col text-[14px] gap-4">
-                <p className="font-semibold">I'm a big believer in the power of knowledge. </p>
+                <p className="font-semibold">I am a big believer in the power of knowledge. </p>
                 <p>When I started my own company from scratch I started to educate myself on business, its processes and also the value in knowledge that is not related to hard skills strictly.</p>
-                <p>The courses resolving around B2B marketing and Sales gave an opportunity to better understand the clients needs, and that's why I can manage to talk to clients about their business, since building software is just a leverge tool to scale.</p>
+                <p>The courses resolving around B2B marketing and Sales gave an opportunity to better understand the clients needs, and that is why I can manage to talk to clients about their business, since building software is just a leverge tool to scale.</p>
             </div>
             <CertificatesWrapper content={Certificates} setPopup={setPopup}/>
             <Popup popup={popup} setPopup={setPopup}/>
